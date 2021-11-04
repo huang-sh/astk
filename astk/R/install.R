f559428011bff94bec8860fa0603f1b9eb384ffc
huang-sh <hsh-me@outlook.com>
2021-11-04 14:41:08 +0800

    add package

--- astk/R/install.R
+++ astk/R/install.R
@@ -12,7 +12,7 @@ if (requirement == "1"){
     ##----->> Install packages from Cran
     options(repos=structure(c(CRAN="https://mirrors.tuna.tsinghua.edu.cn/CRAN/"))) 
 
-    cran.package.list <- c("tidyverse", "ggplot2", "argparse", "UpSetR", "eoffice")
+    cran.package.list <- c("tidyverse", "ggplot2", "argparse", "UpSetR", "eoffice", "ggnewscale")
 
     for(i in cran.package.list){
     if(!(i %in% rownames(installed.packages()))){
@@ -48,6 +48,7 @@ if (OrgDb != "0"){
 }
 
 if (cran != "0"){
+    options(repos=structure(c(CRAN="https://mirrors.tuna.tsinghua.edu.cn/CRAN/"))) 
     install.packages(cran)
 }
 
