6974f22298c32b98ce7a6ae4600f8856c78e3f9e
huang-sh <hsh-me@outlook.com>
2021-11-19 14:11:51 +0800

    re-write,make it general

--- astk/R/upset.R
+++ astk/R/upset.R
@@ -6,9 +6,6 @@ parser <- fig_cmd_parser()
 
 parser$add_argument("--file", nargs='+', help="file names")
 parser$add_argument("--name", nargs='+', help="file path")
-parser$add_argument("--dg", action="store_true", default=F, help="dpsi group")
-
-
 
 
 args <- parser$parse_args()
@@ -16,29 +13,11 @@ args <- parser$parse_args()
 files <- args$file
 names(files) <- args$name
 
-if (args$dg){
-    rep_num <- 2
-    dpsi_ls_n <- lapply(files, function(x){
-        dpsi <- readr::read_tsv(x, skip = 1, 
-                    col_names = c("event_id", "dPSI", "pval"),
-                    col_types = readr::cols("c", "d", "d"))
-        pos <- dpsi[dpsi$dPSI>0, ]$event_id
-        neg <- dpsi[dpsi$dPSI<0, ]$event_id
-        list(pos=pos, neg=neg)
-    })
-
-    dpsi_ls <- unlist(dpsi_ls_n, recursive = F)
-
-} else {
-    rep_num <- 1
-    dpsi_ls <- lapply(files, function(x){
-        dpsi <- readr::read_tsv(x, skip = 1, 
-                    col_names = c("event_id", "dPSI", "pval"),
-                    col_types = readr::cols("c", "d", "d"))
-        dpsi$event_id
-    })
-}
-
+evnet_id.ls <- lapply(files, function(x){
+    df <- readr::read_tsv(x, skip = 1)
+    colnames(df)[1] <- "event_id"
+    df$event_id
+})
 
 pre_colors <- c("#a1d9f9", "#d69bc5", "#ff9999", "#ffc799", "#99d1b8")
 
@@ -46,13 +25,13 @@ colors <- pre_colors[1:length(files)]
 colors[is.na(colors)] <- "black"
 
 
-p <- UpSetR::upset(UpSetR::fromList(dpsi_ls), 
+p <- UpSetR::upset(UpSetR::fromList(evnet_id.ls), 
                    nsets          = length(files), 
                    keep.order     = TRUE, 
-                   sets           = names(dpsi_ls), 
+                   sets           = names(evnet_id.ls), 
                    matrix.color   = "red", 
                    main.bar.color = "black",
-                   sets.bar.color = rep(colors, each=rep_num))
+                   sets.bar.color = colors)
 
 
 save_fig(p, 
