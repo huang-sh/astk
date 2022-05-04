7e2be35e9f8ac87d3d1ee489c0c506d9ab34fa8e
huang-sh <hsh-me@outlook.com>
2022-05-04 17:42:52 +0800

    add dependency

--- astk/R/install.R
+++ astk/R/install.R
@@ -24,8 +24,7 @@ if (args$mirror){
 }
 
 if (args$requirement){
-    ## Install packages of dependency
-    ##----->> Install packages from Cran
+    ## Install packages from Cran
 
     cran.packages <- c("tidyverse", "ggplot2", "argparse", 
                        "UpSetR", "eoffice", "ggnewscale", 
@@ -38,11 +37,11 @@ if (args$requirement){
     } else 
         next
     }
-    ##----->> Install packages from Bioconductor
+    ## Install packages from Bioconductor
 
     bioconductor.packages <- c('ComplexHeatmap', 'clusterProfiler', 'org.Mm.eg.db',
              'org.Hs.eg.db', 'simplifyEnrichment', 'universalmotif', 'memes',"tximport",
-             "DESeq2")
+             "DESeq2", "metagene2")
 
     if (!requireNamespace("BiocManager", quietly = TRUE))
         install.packages("BiocManager")
