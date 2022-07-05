2aa558830593eb13dd05bd6437b81cf1172ecc6a
huang-sh <hsh-me@outlook.com>
2022-07-05 15:59:31 +0800

    add a file for param type setting

---
+++ astk/types.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+astk.types
+~~~~~~~~~~~~~~~~~
+This module stores param type.
+"""
+from pathlib import Path
+from typing import TypeVar, Dict, Iterable, Tuple, Sequence
+
+
+FilePath = TypeVar('FilePath', str, Path)
