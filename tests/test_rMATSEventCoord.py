fec001aa0797e8ccf72d39aff2e3b263a7655b4b
huang-sh <hsh-me@outlook.com>
2022-09-02 17:42:39 +0800

    test EventCoord

--- tests/test_rMATSEventCoord.py
+++ tests/test_rMATSEventCoord.py
@@ -1,48 +1,55 @@
 from astk.utils import rMATSEventCoord 
-
+from astk.constant import rMATS_POS_COLS
+import pandas as pd
 
 def test_sss_rMATSEventCoord():
-    A3SS = rMATSEventCoord("data/A3SS.MATS.JC.txt")
-    A3SS_ss_dic = A3SS.get_all_flank_bed(sss=True)
-    assert list(A3SS_ss_dic.keys()) == ["A0_3SS", "A1_5SS", "A2_3SS", "A3_3SS", "A4_5SS"]
-    for key, df in A3SS_ss_dic.items():
-        if key.endswith("3SS"):
-            assert set(df["end"] - df["start"]) == {23}
-        if key.endswith("5SS"):
-            assert set(df["end"] - df["start"]) == {9}
 
-    A5SS = rMATSEventCoord("data/A5SS.MATS.JCEC.txt")
-    A5SS_ss_dic = A5SS.get_all_flank_bed(sss=True)
-    assert list(A5SS_ss_dic.keys()) == ["A0_3SS", "A1_5SS", "A2_5SS", "A3_3SS", "A4_5SS"]
-    for key, df in A5SS_ss_dic.items():
-        if key.endswith("3SS"):
-            assert set(df["end"] - df["start"]) == {23}
-        if key.endswith("5SS"):
-            assert set(df["end"] - df["start"]) == {9}
+    files = {
+        "A3SS": "data/A3SS.MATS.JC.txt",
+        "A5SS": "data/A5SS.MATS.JCEC.txt",
+        "SE": "data/SE.MATS.JC.txt",
+        "RI": "data/RI.MATS.JCEC.txt",
+        "MXE":"data/MXE.MATS.JC.txt"
+    }
+    etype_ss_names = {
+        "A3SS": ["A0_3SS", "A1_5SS", "A2_3SS", "A3_3SS", "A4_5SS"],
+        "A5SS": ["A0_3SS", "A1_5SS", "A2_5SS", "A3_3SS", "A4_5SS"],
+        "SE": ["A0_3SS", "A1_5SS", "A2_3SS", "A3_5SS", "A4_3SS", "A5_5SS"],
+        "RI": ["A0_3SS", "A1_5SS", "A2_3SS", "A3_5SS"],
+        "MXE": ["A0_3SS", "A1_5SS", "A2_3SS", "A3_5SS", "A4_3SS", "A5_5SS", "A6_3SS", "A7_5SS"]
+    }
+    for etype, file in files.items():
+        dpsi_df = pd.read_csv(file, sep="\t", index_col=0)
+        dpsi_df.drop_duplicates(inplace=True)
+        dpsi_df.dropna(inplace=True)
 
-    SE = rMATSEventCoord("data/SE.MATS.JC.txt")
-    SE_ss_dic = SE.get_all_flank_bed(sss=True)
-    assert list(SE_ss_dic.keys()) == ["A0_3SS", "A1_5SS", "A2_3SS", "A3_5SS", "A4_3SS", "A5_5SS"]
-    for key, df in SE_ss_dic.items():
-        if key.endswith("3SS"):
-            assert set(df["end"] - df["start"]) == {23}
-        if key.endswith("5SS"):
-            assert set(df["end"] - df["start"]) == {9}
+        # tansform 1-based index for unified management
+        if etype == "SE":
+            end_col = ["upstreamEE", "exonEnd", "downstreamEE"]
+        elif etype in ["A5SS", "A3SS"]:
+            end_col = ["longExonEnd", "flankingEE", "shortEE"]
+        elif etype == "MXE":
+            end_col = ["upstreamEE", "1stExonEnd", "2ndExonEnd", "downstreamEE"]
+        elif etype == "RI":
+            end_col = ["upstreamEE", "downstreamEE"]
+        dpsi_df.loc[:, end_col] -= 1
 
-    RI = rMATSEventCoord("data/RI.MATS.JCEC.txt")
-    RI_ss_dic = RI.get_all_flank_bed(sss=True)
-    assert list(RI_ss_dic.keys()) == ["A0_3SS", "A1_5SS", "A2_3SS", "A3_5SS"]
-    for key, df in RI_ss_dic.items():
-        if key.endswith("3SS"):
-            assert set(df["end"] - df["start"]) == {23}
-        if key.endswith("5SS"):
-            assert set(df["end"] - df["start"]) == {9}
+        ps_df = dpsi_df.loc[dpsi_df["strand"] == "+", ]
+        ns_df = dpsi_df.loc[dpsi_df["strand"] == "-", ]
 
-    MXE = rMATSEventCoord("data/MXE.MATS.JC.txt")
-    MXE_ss_dic = MXE.get_all_flank_bed(sss=True)
-    assert list(MXE_ss_dic.keys()) == ["A0_3SS", "A1_5SS", "A2_3SS", "A3_5SS", "A4_3SS", "A5_5SS", "A6_3SS", "A7_5SS"]
-    for key, df in MXE_ss_dic.items():
-        if key.endswith("3SS"):
-            assert set(df["end"] - df["start"]) == {23}
-        if key.endswith("5SS"):
-            assert set(df["end"] - df["start"]) == {9}
+        eventcoor = rMATSEventCoord(file)
+        df_dic = eventcoor.get_all_flank_bed(sss=True)
+        assert list(df_dic.keys()) == etype_ss_names[etype]
+        for idx,( key, df) in enumerate(df_dic.items()):
+            if key.endswith("3SS"):
+                assert set(df["end"] - df["start"]) == {23}
+                assert all(ps_df.loc[:, rMATS_POS_COLS[etype]["+"][idx]] - 20 == df.loc[ps_df.index, "start"])
+                assert all(ns_df.loc[:, rMATS_POS_COLS[etype]["-"][idx]] - 2 == df.loc[ns_df.index, "start"])                
+                assert all(ps_df.loc[:, rMATS_POS_COLS[etype]["+"][idx]] + 2 == df.loc[ps_df.index, "end"]) -1
+                assert all(ns_df.loc[:, rMATS_POS_COLS[etype]["-"][idx]] + 20 == df.loc[ns_df.index, "end"]) -1
+            if key.endswith("5SS"):
+                assert set(df["end"] - df["start"]) == {9}
+                assert all(ps_df.loc[:, rMATS_POS_COLS[etype]["+"][idx]] - 2 == df.loc[ps_df.index, "start"])
+                assert all(ns_df.loc[:, rMATS_POS_COLS[etype]["-"][idx]] - 6 == df.loc[ns_df.index, "start"]) 
+                assert all(ps_df.loc[:, rMATS_POS_COLS[etype]["+"][idx]] + 6 == df.loc[ps_df.index, "end"]) -1
+                assert all(ns_df.loc[:, rMATS_POS_COLS[etype]["-"][idx]] + 2 == df.loc[ns_df.index, "end"]) -1
