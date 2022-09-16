385c76edd10e2afa2d295bf4cdd1276471c50562
huang-sh <hsh-me@outlook.com>
2022-09-16 15:44:50 +0800

    update

--- astk/epi/__init__.py
+++ astk/epi/__init__.py
@@ -6,5 +6,5 @@ The module includes epigenetic feature analysis method
 """
 # from ._func import epi_sc
 # from ._func import epi_profile
-from ._func import signal_heatmap
+from ._func import signal_heatmap, signal_metaplot
 # from ._func import sigcmp
