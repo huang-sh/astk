74ddddc2250bd3d3643063a265271b7b9dd6f940
huangsh <hsh-me@outlook.com>
2023-01-16 13:21:08 +0800

    add aliases.ini

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=find_packages(),    
     include_package_data=True,
     package_data = {
-    '': ['data/motif/*/*.meme', "data/motif/ELM/*", "data/maxent/*"],
+    '': ['data/motif/*/*.meme', "data/motif/ELM/*", "data/maxent/*", "cli/aliases.ini"],
     },
     install_requires=[
         "click>=8.0.0",
