8e7beb94d88a0d3fb9601ffea1aa1b94b4a157cb
huangsh <hsh-me@outlook.com>
2023-01-23 19:34:36 +0800

    update

--- astk/utils/_getfasta.py
+++ astk/utils/_getfasta.py
@@ -6,11 +6,12 @@ def get_coor_fa(df, fasta, output, strandedness=False, rna=False):
     genes = Fasta(fasta)
     with open(output, "w") as f:
         for row in df.itertuples():
-            seq = genes[row[1]][row[2]:row[3]]            
+            seq = genes[row[1]][row[2]:row[3]]
             if strandedness and row[6] == "-":
                 seq = -seq
             if rna:
                 seq.seq = seq.seq.replace("T", "U")
-            f.write(">" + seq.fancy_name + "\n")
+                seq.seq = seq.seq.replace("t", "u")
+            f.write(f">{seq.fancy_name}\n")
             f.write(seq.seq + "\n")
     return output
