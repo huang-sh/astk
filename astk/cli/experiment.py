72593158989e93d37a60875d86a47abe1ea32511
huangsh <hsh-me@outlook.com>
2023-03-09 15:21:32 +0800

    update

--- astk/cli/experiment.py
+++ astk/cli/experiment.py
@@ -8,21 +8,16 @@ from .config import *
 import astk.utils._cli_func as ul
 from astk import draw
 
-# import importlib
-# importlib.set_lazy_imports()
-
-
-
 
 @cli_fun.command(name="vcor", help="correlation heatmap")
 @click.option('-i','--input', 'files', cls=MultiOption, type=click.Path(exists=True),
                 required=True , help="input files")
 @click.option('-o', '--output', type=click.Path(), help="output path")
 @click.option('-m', '--method', type=click.Choice(['pearson', 'kendall', 'spearman']), 
-                default="spearman", help="method of correlation")
-@click.option('-fw', '--width', default=6, help="figure width, default=6 inches")
-@click.option('-fh', '--height', default=6, help="figure height, default=6 inches")
-@click.option('-cmap', '--colormap', default="RdYlBu", help="matplotlib colormap name")
+                default="spearman", show_default=True, help="method of correlation")
+@click.option('-fw', '--width', default=6, show_default=True, help="figure width")
+@click.option('-fh', '--height', default=6, show_default=True, help="figure height")
+@click.option('-cmap', '--colormap', default="RdYlBu", show_default=True, help="matplotlib colormap name")
 @click.option('-title', '--title', default="Correlation Plot", help="figure title")
 @click.option('-gl', '--groupLabel', cls=MultiOption, help="label prefix for multiple files")
 def sub_vcor(*args, **kwargs):
@@ -34,4 +29,3 @@ def sub_vcor(*args, **kwargs):
         BadParameter(msg)
     
     draw.plot_cor_heatmap(**kwargs)
-
