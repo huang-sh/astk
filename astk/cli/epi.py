580740b89e84f244b3a8e850b6f78b256f59c603
huang-sh <hsh-me@outlook.com>
2023-04-30 22:22:45 +0800

    rm epi heatmap and comparsion of deeptools and R code

--- astk/cli/epi.py
+++ astk/cli/epi.py
@@ -4,66 +4,6 @@ import astk.utils as ul
 from .config import *
 from astk import epi
 
-plotType_help_text = """
-lines" will plot the profile line based on the average type selected. "fill" fills the region between zero and the profile curve. The fill in color is semi transparent to
-distinguish different profiles. "se" and "std" color the region between the profile and the standard error or standard deviation of the data. (default: lines)")
-"""
-
-@cli_fun.command(name="signalProfile", help="signal profile withing splicing sites; short alias: sp")
-@click.option('-o', '--output', required=True, type=click.Path(),
-                help="output path")
-@click.option('-e', '--eventFile', "event_file", type=click.Path(exists=True),
-                help="AS event file that including AS event id")
-@click.option('-bw', '--bwFile', "bw_files", cls=MultiOption, type=click.Path(exists=True),
-                help="AS event files that including AS event id")
-@click.option('-bs', '--binSize', "bin_size", default=5, type=int, 
-                help="bin size, default=5")
-@click.option('-uw', '--upStreamWidth', "ups_width", default=150, type=int, 
-                help="upstream width, default=150")
-@click.option('-dw', '--downStreamWidth', "dws_width", default=150, type=int, 
-                help="downstream width, default=150")
-@click.option('-si', '--siteIndex', "sites", cls=MultiOption, type=int,
-                help="splice site index")
-@click.option('-p', '--process', "threads", default=4, type=int, 
-                help="running processors, default=4")
-@click.option('--plotType', 'plot_type', type=click.Choice(['lines',"fill","se",'std']), 
-                default="lines", help=plotType_help_text)
-@click.option('--colorMap', 'color_map', default="RdYlBu", help="Color map to use for the heatmap.")
-@click.option('-fmt', '--figureFormat', "plot_format", type=click.Choice(['auto', 'png',"pdf","svg",'plotly']), 
-                default="auto", help="Image format type")
-@click.option('--height', 'fig_height', default=28, type=int, 
-                help="plot height in cm, default=28")
-@click.option('--width', 'fig_width', default=4, type=int, 
-                help="plot width in cm, default=4")
-@click.option('-ssl', '--splicingSiteLabel', "regionsLabel", cls=MultiOption, 
-                help="splicing site labels")
-@click.option('--samplesLabel', 'samplesLabel', cls=MultiOption, 
-                help="bw samples labels")
-def signal_profile(*args, **kwargs):
-    if kwargs["plot_format"] == "auto":
-        kwargs["plot_format"] = ul.sniff_fig_fmt(kwargs["output"], fmts=['png',"pdf","svg",'plotly'])       
-    epi.signal_heatmap(*args, **kwargs)
-
-
-@cli_fun.command(name="sp2", help="signal profile comparision")
-@click.option('-o', '--output', required=True, type=click.Path(),
-                help="output path")
-@click.option('-mat', 'mat_file', type=click.Path(exists=True), cls=MultiOption,
-                help="matrix file from signalProfile")
-@click.option('-n', '--name', default="signal", help="name")
-@click.option('-gn', '--groupName', cls=MultiOption, type=str, help="event group name")
-@click.option('--height', 'fig_height', default=8, type=int, 
-                help="plot height in cm, default=28")
-@click.option('--width', 'fig_width', default=8, type=int, 
-                help="plot width in cm, default=4")
-@click.option('-fmt', '--figureFormat', "fig_format", type=click.Choice(['auto', 'png',"pdf"]), 
-                default="auto", help="Image format type")
-@click.option('-res', '--resolution', default=72, help="resolution, default=72 ppi")                
-def signal_profile2(*args, **kwargs):
-    if kwargs["fig_format"] == "auto":
-        kwargs["fig_format"] = ul.sniff_fig_fmt(kwargs["output"], fmts=['png',"pdf"])       
-    epi.signal_metaplot(*args, **kwargs)
-
 
 @cli_fun.command(name="signalExtract", help="Extract bigwig signal of splice sites; short alias: se")
 @click.option('-e', '--event', type=click.Path(exists=True), required=True, 
